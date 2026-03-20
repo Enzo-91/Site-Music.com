e4c19847fab0ac9ad4b671a835b5aa8d5d47ac8d
Beauvais <76446435+Enzo-91@users.noreply.github.com>
2026-03-20 14:59:57 +0100

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,693 @@
+
+//1 
+ const audio = document.getElementById("noteA1");
+const bouton = document.getElementById("A1");
+
+function activerSon1() {
+    audio.currentTime = 0; 
+    audio.play();
+    bouton.classList.add('pressed');
+
+    audio.onended = () => bouton.classList.remove('pressed');
+    setTimeout(() => {
+    bouton.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton.addEventListener('click', activerSon1);
+
+
+//2 
+const audio2 = document.getElementById("noteA2");
+const bouton2 = document.getElementById("A2");
+
+function activerSon2() {
+    audio2.currentTime = 0; 
+    audio2.play();
+    bouton2.classList.add('pressed');
+
+    audio2.onended = () => bouton2.classList.remove('pressed');
+    setTimeout(() => {
+    bouton2.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton2.addEventListener('click', activerSon2);
+
+
+
+//3
+const audio3 = document.getElementById("noteA3");
+const bouton3 = document.getElementById("A3");
+
+function activerSon3() {
+    audio3.currentTime = 0; 
+    audio3.play();
+    bouton3.classList.add('pressed');
+
+
+    audio3.onended = () => bouton3.classList.remove('pressed');
+    setTimeout(() => {
+    bouton3.clasList.remove('pressed');
+    }, 1000);
+}
+
+bouton3.addEventListener('click', activerSon3);
+
+
+//4
+const audio4 = document.getElementById("noteA4");
+const bouton4 = document.getElementById("A4");
+
+function activerSon4() {
+    audio4.currentTime = 0; 
+    audio4.play();
+    bouton4.classList.add('pressed');
+
+
+    audio4.onended = () => bouton4.classList.remove('pressed');
+    setTimeout(() => {
+    bouton4.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton4.addEventListener('click', activerSon4);
+
+//5
+const audio5 = document.getElementById("noteA5");
+const bouton5 = document.getElementById("A5");
+
+function activerSon5() {
+    audio5.currentTime = 0; 
+    audio5.play();
+    bouton5.classList.add('pressed');
+
+
+
+    audio5.onended = () => bouton5.classList.remove('pressed');
+    setTimeout(() => {
+    bouton5.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton5.addEventListener('click', activerSon5);
+
+//6
+const audio6 = document.getElementById("noteA6");
+const bouton6 = document.getElementById("A6");
+
+function activerSon6() {
+    audio6.currentTime = 0; 
+    audio6.play();
+    bouton6.classList.add('pressed');
+
+
+    audio6.onended = () => bouton6.classList.remove('pressed');
+    setTimeout(() => {
+    bouton6.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton6.addEventListener('click', activerSon6);
+
+
+//7
+const audio7 = document.getElementById("noteA7");
+const bouton7 = document.getElementById("A7");
+
+function activerSon7() {
+    audio7.currentTime = 0; 
+    audio7.play();
+    bouton7.classList.add('pressed');
+
+
+    audio7.onended = () => bouton7.classList.remove('pressed');
+    setTimeout(() => {
+    bouton7.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton7.addEventListener('click', activerSon7);
+
+
+
+//8
+const audio8 = document.getElementById("noteA8");
+const bouton8 = document.getElementById("A8");
+
+function activerSon8() {
+    audio8.currentTime = 0; 
+    audio8.play();
+    bouton8.classList.add('pressed');
+
+
+    audio8.onended = () => bouton8.classList.remove('pressed');
+    setTimeout(() => {
+    bouton8.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton8.addEventListener('click', activerSon8);
+
+
+//9
+const audio9 = document.getElementById("noteA9");
+const bouton9 = document.getElementById("A9");
+
+function activerSon9() {
+    audio9.currentTime = 0; 
+    audio9.play();
+    bouton9.classList.add('pressed');
+
+
+    audio9.onended = () => bouton9.classList.remove('pressed');
+    setTimeout(() => {
+    bouton9.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton9.addEventListener('click', activerSon9);
+
+
+
+//10
+const audio10 = document.getElementById("noteA10");
+const bouton10 = document.getElementById("A10");
+
+function activerSon10() {
+    audio10.currentTime = 0; 
+    audio10.play();
+    bouton10.classList.add('pressed');
+
+
+    audio10.onended = () => bouton10.classList.remove('pressed');
+    setTimeout(() => {
+    bouton10.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton10.addEventListener('click', activerSon10);
+
+
+
+
+//11
+const audio11 = document.getElementById("noteA11");
+const bouton11 = document.getElementById("A11");
+
+function activerSon11() {
+    audio11.currentTime = 0; 
+    audio11.play();
+    bouton11.classList.add('pressed');
+
+
+    audio11.onended = () => bouton11.classList.remove('pressed');
+    setTimeout(() => {
+    bouton11.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton11.addEventListener('click', activerSon11);
+
+
+
+//12
+const audio12 = document.getElementById("noteA12");
+const bouton12 = document.getElementById("A12");
+
+function activerSon12() {
+    audio12.currentTime = 0; 
+    audio12.play();
+    bouton12.classList.add('pressed');
+
+
+    audio12.onended = () => bouton12.classList.remove('pressed');
+    setTimeout(() => {
+    bouton12.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton12.addEventListener('click', activerSon12);
+
+
+//13
+const audio13 = document.getElementById("noteA13");
+const bouton13 = document.getElementById("A13");
+
+function activerSon13() {
+    audio13.currentTime = 0; 
+    audio13.play();
+    bouton13.classList.add('pressed');
+
+    audio13.onended = () => bouton13.classList.remove('pressed');
+    setTimeout(() => {
+    bouton13.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton13.addEventListener('click', activerSon13);
+
+
+
+//14
+const audio14 = document.getElementById("noteA14");
+const bouton14 = document.getElementById("A14");
+
+
+function activerSon14() {
+    audio14.currentTime = 0; 
+    audio14.play();
+    bouton14.classList.add('pressed');
+
+    audio14.onended = () => bouton14.classList.remove('pressed');
+    setTimeout(() => {
+    bouton14.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton14.addEventListener('click', activerSon14);
+
+
+//15 
+const audio15 = document.getElementById("noteA15");
+const bouton15 = document.getElementById("A15");
+
+function activerSon15() {
+    audio15.currentTime = 0; 
+    audio15.play();
+    bouton15.classList.add('pressed');
+
+
+    audio15.onended = () => bouton15.classList.remove('pressed');
+    setTimeout(() => {
+    bouton15.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton15.addEventListener('click', activerSon15);
+
+
+//16 
+const audio16 = document.getElementById("noteA16");
+const bouton16 = document.getElementById("A16");
+
+function activerSon16() {
+    audio16.currentTime = 0; 
+    audio16.play();
+    bouton16.classList.add('pressed');
+
+
+    audio16.onended = () => bouton16.classList.remove('pressed');
+    setTimeout(() => {
+    bouton16.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton16.addEventListener('click', activerSon16);
+
+
+//17 
+const audio17 = document.getElementById("noteA17");
+const bouton17 = document.getElementById("A17");
+
+function activerSon17() {
+    audio17.currentTime = 0; 
+    audio17.play();
+    bouton17.classList.add('pressed');
+
+
+    audio17.onended = () => bouton17.classList.remove('pressed');
+    setTimeout(() => {
+    bouton17.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton17.addEventListener('click', activerSon17);
+
+
+
+//18 
+const audio18 = document.getElementById("noteA18");
+const bouton18 = document.getElementById("A18");
+
+function activerSon18() {
+    audio18.currentTime = 0; 
+    audio18.play();
+    bouton18.classList.add('pressed');
+
+    audio18.onended = () => bouton18.classList.remove('pressed');
+    setTimeout(() => {
+    bouton18.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton18.addEventListener('click', activerSon18);
+
+
+
+//19
+const audio19 = document.getElementById("noteA19");
+const bouton19 = document.getElementById("A19");
+
+function activerSon19() {
+    audio19.currentTime = 0; 
+    audio19.play();
+    bouton19.classList.add('pressed');
+
+
+    audio19.onended = () => bouton19.classList.remove('pressed');
+    setTimeout(() => {
+    bouton19.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton19.addEventListener('click', activerSon19);
+
+
+
+//20
+const audio20 = document.getElementById("noteA20");
+const bouton20 = document.getElementById("A20");
+
+function activerSon20() {
+    audio20.currentTime = 0; 
+    audio20.play();
+    bouton20.classList.add('pressed');
+
+    audio20.onended = () => bouton20.classList.remove('pressed');
+    setTimeout(() => {
+    bouton20.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton20.addEventListener('click', activerSon20);
+
+
+//21
+const audio21 = document.getElementById("noteA21");
+const bouton21 = document.getElementById("A21");
+
+function activerSon21() {
+    audio21.currentTime = 0; 
+    audio21.play();
+    bouton21.classList.add('pressed');
+
+    audio21.onended = () => bouton21.classList.remove('pressed');
+    setTimeout(() => {
+    bouton21.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton21.addEventListener('click', activerSon21);
+
+
+
+//22
+const audio22 = document.getElementById("noteA22");
+const bouton22 = document.getElementById("A22");
+
+function activerSon22() {
+    audio22.currentTime = 0; 
+    audio22.play();
+    bouton22.classList.add('pressed');
+
+    audio22.onended = () => bouton22.classList.remove('pressed');
+    setTimeout(() => {
+    bouton22.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton22.addEventListener('click', activerSon22);
+
+
+
+//23
+const audio23 = document.getElementById("noteA23");
+const bouton23 = document.getElementById("A23");
+
+function activerSon23() {
+    audio23.currentTime = 0; 
+    audio23.play();
+    bouton23.classList.add('pressed')
+
+    audio23.onended = () => bouton23.classList.remove('pressed');
+    setTimeout(() => {
+    bouton23.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton23.addEventListener('click', activerSon23);
+
+
+
+//24 
+const audio24 = document.getElementById("noteA24");
+const bouton24 = document.getElementById("A24");
+
+function activerSon24() {
+    audio24.currentTime = 0; 
+    audio24.play();
+    bouton24.classList.add('pressed');
+
+
+    audio24.onended = () => bouton24.classList.remove('pressed');
+    setTimeout(() => {
+    bouton24.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton24.addEventListener('click', activerSon24);
+
+
+//25
+const audio25 = document.getElementById("noteA25");
+const bouton25 = document.getElementById("A25");
+
+function activerSon25() {
+    audio25.currentTime = 0; 
+    audio25.play();
+    bouton25.classList.add('pressed');
+
+
+    audio25.onended = () => bouton25.classList.remove('pressed');
+    setTimeout(() => {
+    bouton25.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton25.addEventListener('click', activerSon25);
+
+
+
+//26
+const audio26 = document.getElementById("noteA26");
+const bouton26 = document.getElementById("A26");
+
+function activerSon26() {
+    audio26.currentTime = 0; 
+    audio26.play();
+    bouton26.classList.add('pressed');
+
+    audio26.onended = () => bouton26.classList.remove('pressed');
+    setTimeout(() => {
+    bouton26.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton26.addEventListener('click', activerSon26);
+
+
+
+//27
+const audio27 = document.getElementById("noteA27");
+const bouton27 = document.getElementById("A27");
+
+function activerSon27() {
+    audio27.currentTime = 0; 
+    audio27.play();
+    bouton27.classList.add('pressed');
+
+
+    audio27.onended = () => bouton27.classList.remove('pressed');
+    setTimeout(() => {
+    bouton27.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton27.addEventListener('click', activerSon27);
+
+
+
+//1 
+const audio28 = document.getElementById("noteA28");
+const bouton28 = document.getElementById("A28");
+
+
+function activerSon() {
+    audio28.currentTime = 0; 
+    audio28.play();
+    bouton28.classList.add('pressed');
+
+    audio28.onended = () => bouton28.classList.remove('pressed');
+    setTimeout(() => {
+    bouton28.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton28.addEventListener('click', activerSon);
+
+
+
+//29
+const audio29 = document.getElementById("noteA29");
+const bouton29 = document.getElementById("A29");
+
+function activerSon29() {
+    audio29.currentTime = 0; 
+    audio29.play();
+    bouton29.classList.add('pressed');
+
+
+    audio29.onended = () => bouton29.classList.remove('pressed');
+    setTimeout(() => {
+    bouton29.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton29.addEventListener('click', activerSon29);
+
+
+
+//30
+const audio30 = document.getElementById("noteA30");
+const bouton30 = document.getElementById("A30");
+
+function activerSon30() {
+    audio30.currentTime = 0; 
+    audio30.play();
+    bouton30.classList.add('pressed');
+
+    audio30.onended = () => bouton30.classList.remove('pressed');
+    setTimeout(() => {
+    bouton30.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton30.addEventListener('click', activerSon30);
+
+
+
+//31
+const audio31 = document.getElementById("noteA31");
+const bouton31 = document.getElementById("A31");
+
+function activerSon31() {
+    audio31.currentTime = 0; 
+    audio31.play();
+    bouton31.classList.add('pressed');
+
+
+    audio31.onended = () => bouton31.classList.remove('pressed');
+    setTimeout(() => {
+    bouton31.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton31.addEventListener('click', activerSon31);
+
+
+
+//32
+const audio32 = document.getElementById("noteA32");
+const bouton32 = document.getElementById("A32");
+
+function activerSon32() {
+    audio32.currentTime = 0; 
+    audio32.play();
+    bouton32.classList.add('pressed');
+
+    audio32.onended = () => bouton32.classList.remove('pressed');
+    setTimeout(() => {
+    bouton32.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton32.addEventListener('click', activerSon32);
+
+
+
+//33 
+const audio33 = document.getElementById("noteA33");
+const bouton33 = document.getElementById("A33");
+
+function activerSon33() {
+    audio33.currentTime = 0; 
+    audio33.play();
+    bouton33.classList.add('pressed');
+
+    audio33.onended = () => bouton33.classList.remove('pressed');
+    setTimeout(() => {
+    bouton33.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton33.addEventListener('click', activerSon33);
+
+
+
+//34
+const audio34 = document.getElementById("noteA34");
+const bouton34 = document.getElementById("A34");
+
+function activerSon34() {
+    audio34.currentTime = 0; 
+    audio34.play();
+    bouton34.classList.add('pressed');
+
+    audio34.onended = () => bouton34.classList.remove('pressed');
+    setTimeout(() => {
+    bouton34.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton34.addEventListener('click', activerSon34);
+
+
+
+//35
+const audio35 = document.getElementById("noteA35");
+const bouton35 = document.getElementById("A35");
+
+function activerSon35() {
+    audio35.currentTime = 0; 
+    audio35.play();
+    bouton35.classList.add('pressed');
+
+    audio35.onended = () => bouton35.classList.remove('pressed');
+    setTimeout(() => {
+    bouton35.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton35.addEventListener('click', activerSon35);
+
+
+
+//36 
+const audio36 = document.getElementById("noteA36");
+const bouton36 = document.getElementById("A36");
+
+function activerSon36() {
+    audio36.currentTime = 0; 
+    audio36.play();
+    bouton36.classList.add('pressed');
+
+    audio36.onended = () => bouton36.classList.remove('pressed');
+    setTimeout(() => {
+    bouton36.classList.remove('pressed');
+    }, 1000);
+}
+
+bouton36.addEventListener('click', activerSon36);
+
+
